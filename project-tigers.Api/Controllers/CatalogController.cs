7f058753b83a13eb977b1acc7571c5281fc3a67e
Danny Mach <mach.32@osu.edu>
2022-03-25 18:42:54 -0400

    Get iItem by Id

--- project-tigers.Api/Controllers/CatalogController.cs
+++ project-tigers.Api/Controllers/CatalogController.cs
@@ -17,5 +17,15 @@ namespace Project.Tigers.Api.Controllers
            };
            return Ok(items);
        } 
+
+       [HttpGet("{id:int}")]
+       public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt", "Nike", 29.99m);
+            item.Id = id;
+
+            return Ok(item);
+        }
+        
     }
 }
