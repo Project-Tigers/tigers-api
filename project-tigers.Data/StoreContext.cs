96c41890ad5e190f39938545effc8301d245b2ec
NAVYA KOVVURI <navya.kovvuri@gmail.com>
2022-03-28 14:02:04 -0400

    git commit -m adding startup data

--- project-tigers.Data/StoreContext.cs
+++ project-tigers.Data/StoreContext.cs
@@ -1,14 +1,20 @@
 ﻿using Project.Tigers.Domain.Catalog;
 using Microsoft.EntityFrameworkCore;
 
-namespace project.tigers.Data
+namespace Project.Tigers.Data
 {
     public class StoreContext : DbContext
     {
-        public StoreContext(DbContextOptions<StoreContext> options)
+        public StoreContext(DbContextOptions<StoreContext> options) 
             : base(options)
         { }
 
         public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            DbInitializer.Initialize(builder);
+        }
     }
 }
