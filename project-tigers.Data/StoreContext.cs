abe8f1f17e45e5bec38b2f1dfd54601946af812f
NAVYA KOVVURI <navya.kovvuri@gmail.com>
2022-03-27 23:59:29 -0400

    git commit -m create database context

---
+++ project-tigers.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Project.Tigers.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace project.tigers.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
